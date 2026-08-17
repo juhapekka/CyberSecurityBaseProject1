6c4e390f3a2a4338dba0b90f851bbe74f9e47707
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2026-08-17 21:38:16 +0300

    lisätty note + search näkyviin noi käyttäjä + noten omistaja jotta toi screenshot juttu toimii
    Signed-off-by: Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>

--- secapp/views.py
+++ secapp/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.db import connection
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.views.decorators.csrf import csrf_exempt
 from .models import Note
 
